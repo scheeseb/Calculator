7c68e87a12be49ffe6acbc9a1323c9ffb982a6ab
scheeseb <samuel.cheeseboro@gmail.com>
2024-02-21 00:17:57 -0600

    Finished the game. goes indefinietly

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const rockButton = document.querySelector("#rockButton");
+const paperButton = document.querySelector("#paperButton");
+const scissorButton = document.querySelector("#scissorButton")
+
+const playerScoreDisplay = document.querySelector("#playerScore");
+const computerScoreDisplay = document.querySelector("#computerScore");
+const roundCountDisplay = document.querySelector("#roundCount");
+
+let playerScore = 0;
+let computerScore = 0;
+let roundCount = 0;
+
+    rockButton.addEventListener('click', function(){
+        findWinner(computerChoice(), "rock");
+    })
+    paperButton.addEventListener('click', function(){
+        findWinner(computerChoice(), "paper");
+    })
+    scissorButton.addEventListener('click', function(){
+        findWinner(computerChoice(), "scissor");
+    })
+
+
+function printScore(){
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+    roundCountDisplay.textContent = roundCount;
+
+}
+
 function computerChoice() {
     const pick = Math.floor(Math.random() * 3);
     if (pick === 1) { return "rock" }
@@ -5,7 +35,7 @@ function computerChoice() {
     else { return "scissors" }
 }
 
-function playGame(comChoice, userIn) {
+function findWinner(comChoice, userIn) {
     let winStatus = ""
     let userChoice = userIn.toLowerCase();
     if (comChoice === "rock") {
@@ -13,10 +43,14 @@ function playGame(comChoice, userIn) {
             winStatus = "Tied on " + comChoice;
         }
         else if (userChoice === "paper") {
-            winStatus = "Win " + userChoice + " over " + comChoice
+            winStatus = "Win " + userChoice + " over " + comChoice;
+            playerScore++;
+            roundCount++;
         }
         else {
-            winStatus = "Lose " + comChoice + " over " + userChoice
+            winStatus = "Lose " + comChoice + " over " + userChoice;
+            computerScore++;
+            roundCount++;
         }
     }
     else if (comChoice === "paper") {
@@ -25,9 +59,13 @@ function playGame(comChoice, userIn) {
         }
         else if (userChoice === "scissors") {
             winStatus = "Win " + userChoice + " over " + comChoice;
+            playerScore++;
+            roundCount++;
         }
         else {
             winStatus = "Lose " + comChoice + " over " + userChoice;
+            computerScore++;
+            roundCount++;
         }
     }
     else {
@@ -36,35 +74,16 @@ function playGame(comChoice, userIn) {
         }
         else if (userChoice === "rock") {
             winStatus = "Win " + userChoice + " over " + comChoice;
+            playerScore++;
+            roundCount++;
         }
         else {
             winStatus = "Lose " + comChoice + " over " + userChoice;
+            computerScore++;
+            roundCount++;
         }
     }
+    alert(winStatus)
+    printScore()
     return winStatus
-}
-
-function fiveRounds() {
-    for (let i = 0; i < 5; i++) {
-        let userChoice = prompt()
-        let comChoice = computerChoice();
-        let userScore = 0;
-        let comScore = 0;
-        let rawOutcome = playGame(comChoice, userChoice);
-        let outcome = rawOutcome.slice(0,2)
-
-        if(outcome === "Wi"){
-            userScore++
-        }
-        else if(outcome === "Lo"){
-            comScore++
-        }
-        else {
-            i--
-        }
-        console.log(rawOutcome);
-        console.log("user:" + userScore,"computer:" + comScore, "round:" + (i + 1));
-        
-
-    }
 }
